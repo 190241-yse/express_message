3c90b052b522821d7c2df48c620fb82894107894
190241-yse <190241th@yse-c.net>
2020-12-07 11:42:23 +0900

    1207

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const express = require('express');
 //サーバー作成
 const app = express();
 
+// json対応
+app.use(express.json());
+// URL エンコードされたデータを解析する
+app.use(express.urlencoded({ extended: true}));
+
+//クロスドメインの許可
+app.use((req, res, next) => {
+    console.log(`middleware: all. url: ${req.url}`);
+
+    //CROS設定: 全てのドメインに対して許可
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
+});
+
+app.post('/', (req, res) => {
+    let result = {
+        message: req.body.message,
+        datetime: new Date(),
+    };
+    res.send(result);
+});
+
 //ルーティング
 // webルートにリクエストされたらレスポンス
 app.get('/', (req, res) => {
